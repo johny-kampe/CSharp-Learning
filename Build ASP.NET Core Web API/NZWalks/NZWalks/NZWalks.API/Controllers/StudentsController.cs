c3eb11d592e1e60707a5f5d4636b8e5b897e8ac9
John Kamperakis <johnkamperakis@gmail.com>
2024-01-12 21:10:57 +0200

    Section 2 : Running and Calling ASP.NET Core Web API

---
+++ Build ASP.NET Core Web API/NZWalks/NZWalks/NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // https:localhost:<port>/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: https:localhost:<port>/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "John", "Jane", "Mark", "Emily", "David" };
+
+            return Ok(studentNames);
+        }
+    }
+}

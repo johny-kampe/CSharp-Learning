6daba5285c0aac74f96c49be78bc5c38fc488d4a
John Kamperakis <johnkamperakis@gmail.com>
2024-02-03 14:31:24 +0200

    Section 8 : First merge from Section8 branch

--- Build ASP.NET Core Web API/NZWalks/NZWalks/NZWalks.API/obj/Debug/net7.0/NZWalks.API.AssemblyInfo.cs
+++ Build ASP.NET Core Web API/NZWalks/NZWalks/NZWalks.API/obj/Debug/net7.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b42a3fa12d4bc60bd3aa658f885802dd8f8f513d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

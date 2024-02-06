fb97fb339917dbc1322d05c3f17b759dd0b9fe6a
John Kamperakis <johnkamperakis@gmail.com>
2024-02-06 16:52:38 +0200

    Section 8 : Setting Up Identity DbContext

---
+++ Build ASP.NET Core Web API/NZWalks/NZWalks/NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksAuthDbContext: DbContext
+    {
+        public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options): base(options)
+        {
+
+        }
+    }
+}
--- Build ASP.NET Core Web API/NZWalks/NZWalks/NZWalks.API/obj/Debug/net7.0/NZWalks.API.AssemblyInfo.cs
+++ Build ASP.NET Core Web API/NZWalks/NZWalks/NZWalks.API/obj/Debug/net7.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a89ba54440fb914a10c0801c3b79b7b4359a755")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

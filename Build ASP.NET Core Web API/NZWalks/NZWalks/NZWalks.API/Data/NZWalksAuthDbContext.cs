03639630966554c2741410df35dc4476ad101816
John Kamperakis <johnkamperakis@gmail.com>
2024-02-06 16:58:26 +0200

    Section 8 : Seeding Roles

--- Build ASP.NET Core Web API/NZWalks/NZWalks/NZWalks.API/Data/NZWalksAuthDbContext.cs
+++ Build ASP.NET Core Web API/NZWalks/NZWalks/NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using NZWalks.API.Models.Domain;
 
 namespace NZWalks.API.Data
@@ -8,6 +9,31 @@ namespace NZWalks.API.Data
         public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options): base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var readerRuleId = Guid.NewGuid().ToString();
+            var writerRuleId = Guid.NewGuid().ToString();
+
+            var roles = new List<IdentityRole> 
+            { 
+                new IdentityRole
+                {
+                    Id = readerRuleId,
+                    ConcurrencyStamp = readerRuleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },new IdentityRole
+                {
+                    Id = writerRuleId,
+                    ConcurrencyStamp = writerRuleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
         }
     }
 }

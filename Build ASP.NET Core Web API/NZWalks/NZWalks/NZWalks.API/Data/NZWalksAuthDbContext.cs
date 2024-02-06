b9cc023241025c5f79027728b75db0e7d6b401c7
John Kamperakis <johnkamperakis@gmail.com>
2024-02-06 17:05:02 +0200

    Section 8 : Merge Section8 branch to master branch

---
+++ Build ASP.NET Core Web API/NZWalks/NZWalks/NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksAuthDbContext: DbContext
+    {
+        public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options): base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var readerRuleId = Guid.NewGuid().ToString();
+            var writerRuleId = Guid.NewGuid().ToString();
+
+            var roles = new List<IdentityRole> 
+            { 
+                new IdentityRole
+                {
+                    Id = readerRuleId,
+                    ConcurrencyStamp = readerRuleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },new IdentityRole
+                {
+                    Id = writerRuleId,
+                    ConcurrencyStamp = writerRuleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+        }
+    }
+}
--- Build ASP.NET Core Web API/NZWalks/NZWalks/NZWalks.API/Program.cs
+++ Build ASP.NET Core Web API/NZWalks/NZWalks/NZWalks.API/Program.cs
@@ -17,6 +17,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<NZWalksDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString")));
 
+builder.Services.AddDbContext<NZWalksAuthDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksAuthConnectionString")));
+
 builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
 builder.Services.AddScoped<IWalkRepository, SQLWalkRepository>();
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));

ff34f600600af40e499c14ee471757ed7d3f353e
John Kamperakis <johnkamperakis@gmail.com>
2024-02-14 16:38:55 +0100

    Section 9 : Create Auth Controller and Register Action Method

--- Build ASP.NET Core Web API/NZWalks/NZWalks/NZWalks.API/Data/NZWalksAuthDbContext.cs
+++ Build ASP.NET Core Web API/NZWalks/NZWalks/NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -1,32 +1,34 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using NZWalks.API.Models.Domain;
 
 namespace NZWalks.API.Data
 {
-    public class NZWalksAuthDbContext: DbContext
+    public class NZWalksAuthDbContext : IdentityDbContext
     {
-        public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options): base(options)
+        public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
         {
 
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            base.OnModelCreating(modelBuilder);
+            base.OnModelCreating(builder);
 
-            var readerRuleId = Guid.NewGuid().ToString();
-            var writerRuleId = Guid.NewGuid().ToString();
+            var readerRuleId = "855583aa-d7df-472e-bbea-9a6e7bd20b41";
+            var writerRuleId = "5bed93e7-0af0-4fa1-a52f-cee50ec2e10e";
 
-            var roles = new List<IdentityRole> 
-            { 
+            var roles = new List<IdentityRole>
+            {
                 new IdentityRole
                 {
                     Id = readerRuleId,
                     ConcurrencyStamp = readerRuleId,
                     Name = "Reader",
                     NormalizedName = "Reader".ToUpper()
-                },new IdentityRole
+                },
+                new IdentityRole
                 {
                     Id = writerRuleId,
                     ConcurrencyStamp = writerRuleId,
@@ -34,6 +36,8 @@ namespace NZWalks.API.Data
                     NormalizedName = "Writer".ToUpper()
                 }
             };
+
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
--- Build ASP.NET Core Web API/NZWalks/NZWalks/NZWalks.API/Migrations/NZWalksAuthDb/20240214152246_Creating Auth Database.Designer.cs
+++ Build ASP.NET Core Web API/NZWalks/NZWalks/NZWalks.API/Migrations/NZWalksAuthDb/20240214152246_Creating Auth Database.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NZWalks.API.Data;
 
@@ -11,9 +12,11 @@ using NZWalks.API.Data;
 namespace NZWalks.API.Migrations.NZWalksAuthDb
 {
     [DbContext(typeof(NZWalksAuthDbContext))]
-    partial class NZWalksAuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240214152246_Creating Auth Database")]
+    partial class CreatingAuthDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
